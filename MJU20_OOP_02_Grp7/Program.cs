db152d99172533c0000a6bd987a274f5894e7ea8
Rippan <jepperipnas@gmail.com>
2021-04-13 10:15:43 +0200

    Add project files.

---
+++ MJU20_OOP_02_Grp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace MJU20_OOP_02_Grp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MJU20_OOP_02_Grp7/basicMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MJU20_OOP_02_Grp7
+{
+    public class basicMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
