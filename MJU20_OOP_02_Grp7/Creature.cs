c7bfb517e7f84c2defb423b67dec6db92b28f2e8
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-04-14 13:52:44 +0200

    Created entity

--- MJU20_OOP_02_Grp7/Creature.cs
+++ MJU20_OOP_02_Grp7/Creature.cs
@@ -4,19 +4,13 @@ using System.Text;
 
 namespace MJU20_OOP_02_Grp7
 {
-    public abstract class Creature
+    public abstract class Creature : Entity
     {
         public int Hp { get; private set; }
-        public Point Position { get; private set; }
-        public char Symbol { get; private set; }
-        public ConsoleColor Color { get; private set; }
 
-        public Creature(int hp, Point position, char symbol, ConsoleColor color)
+        public Creature(int hp, Point position, char symbol, ConsoleColor color) : base(position, symbol, color)
         {
             Hp = hp;
-            Position = position;
-            Symbol = symbol;
-            Color = color;
         }
 
         public void Damage(int dmg)
@@ -27,11 +21,6 @@ namespace MJU20_OOP_02_Grp7
         public void Heal(int heal)
         {
             Hp += heal;
-        }
-
-        public void Move(Point movement)
-        {
-            Position += movement;
         }
     }
 }
---
+++ MJU20_OOP_02_Grp7/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MJU20_OOP_02_Grp7
+{
+    public class Entity
+    {
+        public Point Position { get; private set; }
+        public char Symbol { get; private set; }
+        public ConsoleColor Color { get; private set; }
+
+        public Entity(Point position, char symbol, ConsoleColor color)
+        {
+            Position = position;
+            Symbol = symbol;
+            Color = color;
+        }
+
+        public void Move(Point movement)
+        {
+            Position += movement;
+        }
+    }
+}
