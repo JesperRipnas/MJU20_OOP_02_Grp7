2781ace747253cac544ac90aceedced823a7e3a8
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-04-14 15:22:13 +0200

    Added attack damage to creature class

--- MJU20_OOP_02_Grp7/Creature.cs
+++ MJU20_OOP_02_Grp7/Creature.cs
@@ -7,10 +7,12 @@ namespace MJU20_OOP_02_Grp7
     public abstract class Creature : Entity
     {
         public int Hp { get; private set; }
+        public int Dmg { get; private set; }
 
-        public Creature(int hp, Point position, char symbol, ConsoleColor color) : base(position, symbol, color)
+        public Creature(int hp, int dmg, Point position, char symbol, ConsoleColor color) : base(position, symbol, color)
         {
             Hp = hp;
+            Dmg = dmg;
         }
 
         public void Damage(int dmg)
