482341d299a49bca63312587cb98d9d31d67993d
Joakim Norén <joakim.noren@live.se>
2021-04-14 13:53:36 +0200

    gjort menu klass

---
+++ MJU20_OOP_02_Grp7/Menu.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MJU20_OOP_02_Grp7
+{
+    public class Menu
+    {
+        public int menuPage;
+
+        private int select;
+
+        private string[] options;
+
+        private string subTitle;
+
+        private string title; 
+
+        public Menu(int menuPage, string[] options, string subTitle, string title) //new menu
+        {
+            this.menuPage = menuPage;
+            this.options = options;
+            this.subTitle = subTitle;
+            this.title = title;
+        }
+        public Menu() // creates the main menu
+        {
+            string[] standardoption = { "Start", "Difficulty", "Score", "Options", "Exit" };
+
+            this.menuPage = 0;
+            this.options = standardoption;
+            this.subTitle = "";
+
+            this.title = @"                                                                   ,;             .,                                                              ,;           
+                                                                 f#i             ,Wt j.                                                  i      f#i j.         
+            ..       :           ..                            .E#t             i#D. EW,                   ..           ;               LE    .E#t  EW,        
+           ,W,     .Et          ;W,      ,##############Wf.   i#W,             f#f   E##j                 ;W,         .DL              L#E   i#W,   E##j       
+          t##,    ,W#t         j##,       ........jW##Wt     L#D.            .D#i    E###D.              j##, f.     :K#L     LWL     G#W.  L#D.    E###D.     
+         L###,   j###t        G###,             tW##Kt     :K#Wfff;         :KW,     E#jG#W;            G###, EW:   ;W##L   .E#f     D#K. :K#Wfff;  E#jG#W;    
+       .E#j##,  G#fE#t      :E####,           tW##E;       i##WLLLLt        t#f      E#t t##f         :E####, E#t  t#KE#L  ,W#;     E#K.  i##WLLLLt E#t t##f   
+      ;WW; ##,:K#i E#t     ;W#DG##,         tW##E;          .E#L             ;#G     E#t  :K#E:      ;W#DG##, E#t f#D.L#L t#K:    .E#E.    .E#L     E#t  :K#E: 
+     j#E.  ##f#W,  E#t    j###DW##,      .fW##D,              f#E:            :KE.   E#KDDDD###i    j###DW##, E#jG#f  L#LL#G     .K#E        f#E:   E#KDDDD###i
+   .D#L    ###K:   E#t   G##i,,G##,    .f###D,                 ,WW;            .DW:  E#f,t#Wi,,,   G##i,,G##, E###;   L###j     .K#D          ,WW;  E#f,t#Wi,,,
+  :K#t     ##D.    E#t :K#K:   L##,  .f####Gfffffffffff;        .D#;             L#, E#t  ;#W:   :K#K:   L##, E#K:    L#W;     .W#G            .D#; E#t  ;#W:  
+  ...      #G      .. ;##D.    L##, .fLLLLLLLLLLLLLLLLLi          tt              jt DWi   ,KK: ;##D.    L##, EG      LE.     :W##########Wt     tt DWi   ,KK: 
+           j          ,,,      .,,                                                              ,,,      .,,  ;       ;@      :,,,,,,,,,,,,,.                  ";
+        }
+
+        private void MainMenu()
+        {
+            Console.SetWindowSize(160,40);
+            ConsoleColor foreground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(title);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(subTitle);
+            Console.WriteLine();
+            Console.WriteLine();
+            DrawOptions();
+        }
+
+        private  void DrawOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                string selcar;
+                if (i == select)
+                {
+                    selcar = ">>";
+                }
+                else
+                {
+                    selcar = "  ";
+                }
+                Console.WriteLine($"{selcar} {options[i]}");
+                Console.WriteLine();
+            }
+            
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            select = 0;
+
+            do
+            {
+                //display menu
+                Console.Clear();
+                MainMenu();
+
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                //update select
+                if (keyPressed == ConsoleKey.W)
+                {
+                    --select;
+                }
+                else if (keyPressed == ConsoleKey.S)
+                {
+                    ++select;
+                }
+
+                if (select == options.Length)
+                {
+                    select = 0;
+                }
+                if (select == -1)
+                {
+                    select = options.Length - 1;
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return select;
+        }
+    }
+}
--- MJU20_OOP_02_Grp7/Program.cs
+++ MJU20_OOP_02_Grp7/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using static System.Console;
 
+
 namespace MJU20_OOP_02_Grp7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            
+            
         }
+
+    
+
     }
+
+
+
+
+
 }
