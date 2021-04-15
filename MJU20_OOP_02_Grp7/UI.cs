7a35d5a8ed4b10e3e52eb99fe28f93449df424ae
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-04-15 15:28:43 +0200

    Updated UI and made methods static

--- MJU20_OOP_02_Grp7/UI.cs
+++ MJU20_OOP_02_Grp7/UI.cs
@@ -6,16 +6,10 @@ namespace MJU20_OOP_02_Grp7
     public class UI
     {
         // view window size
-        private static int _height;
-        private static int _width;
+        public static int height;
+        public static int width;
 
-        public UI(int height, int width)
-        {
-            _height = height;
-            _width = width;
-        }
-
-        public void DrawScreen(char[,] map, Player player, Creature[] entities)
+        public static void DrawScreen(char[,] map, Player player, Entity[] entities)
         {
             DrawMap(map, player.Position);
             DrawEntities(entities);
@@ -25,7 +19,7 @@ namespace MJU20_OOP_02_Grp7
 
         private static void DrawUI(Player player)
         {
-            Console.SetCursorPosition(5, _height + 1);
+            Console.SetCursorPosition(5, height + 1);
             Console.WriteLine($"HP: {player.Hp}");
             // not yet implemented
             //Console.SetCursorPosition(5, _height + 2);
@@ -40,7 +34,7 @@ namespace MJU20_OOP_02_Grp7
             Console.Write(player.Symbol);
         }
 
-        private static void DrawEntities(Creature[] entities)
+        private static void DrawEntities(Entity[] entities)
         {
             foreach (Creature entity in entities)
             {
@@ -50,14 +44,14 @@ namespace MJU20_OOP_02_Grp7
             }
         }
 
-        private void DrawMap(char[,] map, Point playerPosition)
+        private static void DrawMap(char[,] map, Point playerPosition)
         {
             string background = "";
-            for (int y = 0; y < _height; y++)
+            for (int y = 0; y < height; y++)
             {
-                for (int x = 0; x < _width; x++)
+                for (int x = 0; x < width; x++)
                 {
-                    background += map[playerPosition.X - (_width / 2) + x, playerPosition.Y - (_height / 2) + y];
+                    background += map[playerPosition.X - (width / 2) + x, playerPosition.Y - (height / 2) + y];
                 }
                 background += "\n";
             }
