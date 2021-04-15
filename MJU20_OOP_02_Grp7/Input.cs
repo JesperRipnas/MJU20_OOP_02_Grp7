a46d9eeb1e1c1c7db42e4b7648c38d74ed9eda85
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-04-15 09:48:47 +0200

    Updated input

---
+++ MJU20_OOP_02_Grp7/Input.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MJU20_OOP_02_Grp7
+{
+    public class Input
+    {
+
+        public ConsoleKey Readkey()
+        {
+            List<ConsoleKey> key = new List<ConsoleKey>();
+            key.Add(ConsoleKey.NoName);
+            while (Console.KeyAvailable)
+            {
+                key.Add(Console.ReadKey(true).Key);
+            }
+            return key[key.Count - 1];
+        }
+    }
+}
