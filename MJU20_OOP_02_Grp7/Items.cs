92d149d8719b1a9b7711603eaf43b15ee0b06c53
Sebastian <sebastian.jerreling@hotmail.com>
2021-04-16 13:50:16 +0200

    added trapclass and removed dmg from item

--- MJU20_OOP_02_Grp7/Items.cs
+++ MJU20_OOP_02_Grp7/Items.cs
@@ -5,13 +5,12 @@ namespace MJU20_OOP_02_Grp7
 {
     public struct ItemStats
     {
-        public int Heal, Dmg, AttackUp;
+        public int Heal, AttackUp;
         public ConsoleColor Color;
 
-        public ItemStats(int heal, int dmg, int attackUp, ConsoleColor color)
+        public ItemStats(int heal, int attackUp, ConsoleColor color)
         {
             Heal = heal;
-            Dmg = dmg;
             AttackUp = attackUp;
             Color = color;
         }
@@ -22,9 +21,8 @@ namespace MJU20_OOP_02_Grp7
         static public List<Item> activeItems = new List<Item>();
         static public Dictionary<char, ItemStats> itemTypes = new Dictionary<char, ItemStats>()
         {
-            {'♥', new ItemStats(25, 0, 0, ConsoleColor.Red)},
-            {'ʇ', new ItemStats(0, 0, 2, ConsoleColor.Gray)},
-            {'·', new ItemStats (0, 25, 0, ConsoleColor.DarkGray)}
+            {'♥', new ItemStats(25, 0, ConsoleColor.Red)},
+            {'ʇ', new ItemStats(0, 2, ConsoleColor.Gray)}
         };
 
         public Item(char symbol, Point position, ItemStats stats) : base(position, symbol, stats.Color)
