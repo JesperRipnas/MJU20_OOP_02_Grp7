666b784948ae5b61a81f6a26d8eb4f55220ffae2
Olivia Persson <Olivia__P@hotmail.com>
2021-04-21 11:06:03 +0200

    Added comments in entity and unit tests for creature methods

--- MJU20_OOP_02_Grp7/Entity.cs
+++ MJU20_OOP_02_Grp7/Entity.cs
@@ -85,6 +85,7 @@ namespace MJU20_OOP_02_Grp7
         }
         public void Attack()
         {
+            //Creates a list of positions around the player to check for enemies
             List<Point> playerArea = new List<Point>();
             playerArea.Add(new Point(0, 1));
             playerArea.Add(new Point(0, -1));
@@ -97,31 +98,33 @@ namespace MJU20_OOP_02_Grp7
 
             foreach (Point area in playerArea)
             {
+                //Check for enemy in current position
                 Point tempPosition = Position + area;
                 Enemy tempEnemy = null;
                 foreach (Enemy enemy in Enemy.activeEnemies)
                 {
                     if (enemy.Position == tempPosition)
                     {
-                        enemy.Damage(Game.player.Dmg);
+                        enemy.Damage(Game.player.Dmg); //Make damage to enemy
                         enemy.ShowHp = true;
                         enemy.showHpTick = Game.GetTick();
                         //FlickerAsync(enemy);
                         UI.MessageList.Add(new GameMessage(Game.player.Activate(enemy), Game.GetTick() + 10));
                         Point tempEnemyPosition = enemy.Position + area + area;
 
-                        enemy.Move(area, this);
+                        enemy.Move(area, this);  //Move enemy when attacked
 
                         tempEnemy = enemy;
                     }
                 }
+                //
                 if (tempEnemy != null)
                 {
                     if (tempEnemy.Hp <= 0)
                     {
                         Game.player.AddPlayerScore(tempEnemy.Score); // Add score for killing enemy
-                        tempEnemy.ShowHp = false;
-                        Enemy.activeEnemies.Remove(tempEnemy);
+                        tempEnemy.ShowHp = false; //Remove hp when enemy is dead
+                        Enemy.activeEnemies.Remove(tempEnemy); //Remove enemy from list when dead
                         UI.MessageList.Add(new GameMessage($"Enemy {tempEnemy.Symbol} died!, you recieved {tempEnemy.Score} points", Game.GetTick() + 10));
                     }
                 }
