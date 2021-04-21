6b7e8cd47e4d8970f2d2730a99e5f2a403a45e26
Olivia Persson <Olivia__P@hotmail.com>
2021-04-21 11:46:12 +0200

    added comments in attack method

--- MJU20_OOP_02_Grp7/Entity.cs
+++ MJU20_OOP_02_Grp7/Entity.cs
@@ -83,53 +83,6 @@ namespace MJU20_OOP_02_Grp7
                 Position += movement;
             }
         }
-        public void Attack()
-        {
-            //Creates a list of positions around the player to check for enemies
-            List<Point> playerArea = new List<Point>();
-            playerArea.Add(new Point(0, 1));
-            playerArea.Add(new Point(0, -1));
-            playerArea.Add(new Point(1, 0));
-            playerArea.Add(new Point(-1, 0));
-            playerArea.Add(new Point(-1, -1));
-            playerArea.Add(new Point(-1, 1));
-            playerArea.Add(new Point(1, 1));
-            playerArea.Add(new Point(1, -1));
-
-            foreach (Point area in playerArea)
-            {
-                //Check for enemy in current position
-                Point tempPosition = Position + area;
-                Enemy tempEnemy = null;
-                foreach (Enemy enemy in Enemy.activeEnemies)
-                {
-                    if (enemy.Position == tempPosition)
-                    {
-                        enemy.Damage(Game.player.Dmg); //Make damage to enemy
-                        enemy.ShowHp = true;
-                        enemy.showHpTick = Game.GetTick();
-                        //FlickerAsync(enemy);
-                        UI.MessageList.Add(new GameMessage(Game.player.Activate(enemy), Game.GetTick() + 10));
-                        Point tempEnemyPosition = enemy.Position + area + area;
-
-                        enemy.Move(area, this);  //Move enemy when attacked
-
-                        tempEnemy = enemy;
-                    }
-                }
-                //
-                if (tempEnemy != null)
-                {
-                    if (tempEnemy.Hp <= 0)
-                    {
-                        Game.player.AddPlayerScore(tempEnemy.Score); // Add score for killing enemy
-                        tempEnemy.ShowHp = false; //Remove hp when enemy is dead
-                        Enemy.activeEnemies.Remove(tempEnemy); //Remove enemy from list when dead
-                        UI.MessageList.Add(new GameMessage($"Enemy {tempEnemy.Symbol} died!, you recieved {tempEnemy.Score} points", Game.GetTick() + 10));
-                    }
-                }
-            }
-        }
         //Method to get enemy to flicker when attacked
         public async Task FlickerAsync(Enemy enemy)
         {
